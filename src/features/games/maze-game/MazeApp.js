6a411e4c507c65553171df578db3c4ff194144b5
o3ouo <o3ouo@daum.net>
2025-02-20 04:03:58 +0900

    maze

--- src/features/games/maze-game/MazeApp.js
+++ src/features/games/maze-game/MazeApp.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './MazeApp.css';
+import Maze from "./Maze";
 
 function MazeApp() {
+  const [stage, setStage] = useState(1);
+  const [gameWon, setGameWon] = useState(false);
+
+  const nextStage = () => {
+    setGameWon(false);
+    setStage(stage + 1);
+  };
+
   return (
     <div className="maze-wrap">
-      <h1>Maze Game</h1>
+      <h1>Maze Game - Stage {stage}</h1>
+      {gameWon ? (
+        <div>
+          <p className="win-massage"> Stage {stage} Clear!</p>
+          <button type="button" onClick={nextStage}> Next Stage </button>
+        </div>
+      ) : (
+        <Maze stage={stage} onWin={() => setGameWon(true)} />
+      )}
     </div>
   );
 }
